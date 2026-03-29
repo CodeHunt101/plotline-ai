79ff1fe53d54f50f822fec14dc36f04fe4805ef1
Harold Torres <haroldtm55@gmail.com>
2026-03-29 15:58:05 +1100

    feat: integrate @next/bundle-analyzer for improved build analysis and add analyze script to package.json

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+import bundleAnalyzer from "@next/bundle-analyzer";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -11,4 +12,8 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
+export default withBundleAnalyzer(nextConfig);
